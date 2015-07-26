75cfe962fe8e30e8ee1119701c5874e5cf52738c
ciaran_whyte_mtt <ciaran.whyte@mttnow.com>
2015-07-26 08:57:41 +0100

    Task - removing as much jquery as possible
     - Rewriting the lobby.js event functions in v-js
     - Removing unused status bar jquery functions

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -2,8 +2,8 @@ window.onload = function () {
 
   var messages = [];
   var socket = io.connect(location.origin);
-  var field = document.getElementById("field");
-  var readyButton = document.getElementById("readyButton");
+  var messageInput = document.getElementById("field");
+  var sendMessageButton = document.getElementById("send");
   var startGameButton = document.getElementById("startGameButton");
   var content = document.getElementById("content");
   var name = document.getElementById("name");
@@ -39,13 +39,17 @@ window.onload = function () {
         document.getElementById(playerNum).innerText = listOfUsers[i].uuid;
         // Display users Status
         var playerStatus = playerNum;
-        if (listOfUsers[i].uuid == name.value ) {
-          playerStatus =  '<input type="checkbox" id="readyButton"/> Are you Ready?<br/>';
-          if(listOfUsers[i].ready) playerStatus = '<p style="color: green">IM READY</p>';
+        if (listOfUsers[i].uuid == name.value) {
+          playerStatus = '<input type="checkbox" id="readyButton"/> Are you Ready?<br/>';
+          if (listOfUsers[i].ready) playerStatus = '<p style="color: green">IM READY</p>';
           document.getElementById(playerNum + "row").innerHTML = playerStatus;
+          document.getElementById(playerNum + "row").onclick = function () {
+            socket.emit('userReady', room, name.value);
+            document.getElementById("readyButton").disabled = true;
+          };
         } else {
           // Other player
-          if(listOfUsers[i].ready) playerStatus = '<p style="color: green">PLAYER READY</p>';
+          if (listOfUsers[i].ready) playerStatus = '<p style="color: green">PLAYER READY</p>';
           document.getElementById(playerNum + "row").innerHTML = playerStatus;
         }
       }
@@ -65,23 +69,22 @@ window.onload = function () {
   });
 
   socket.on('redirect', function () {
-    window.location.href = location.origin + "/games/view/"+room;
+    window.location.href = location.origin + "/games/view/" + room;
   });
 
-  $(document).on('click', '#send', function () {
-    socket.emit('send', room, {message: field.value, username: name.value});
-    $('#field').val('');
-  });
-  $(document).on('click', '#readyButton', function () {
-    socket.emit('userReady', room, name.value);
-    document.getElementById("readyButton").disabled = true;
-  });
-  $(document).on('click', '#initGameButton', function () {
-    socket.emit('startGame', room, name.value);
-  });
-  $(document).on('keydown', '#field', function () {
+  function submitMessage() {
+    socket.emit('send', room, {message: messageInput.value, username: name.value});
+    messageInput.value = '';
+  }
+
+  sendMessageButton.onclick = submitMessage;
+  messageInput.onkeypress = function () {
     if (event.keyCode == 13) {
-      $('#send').trigger('click');
+      submitMessage();
     }
-  });
+  };
+
+  startGameButton.onclick = function () {
+    socket.emit('startGame', room, name.value);
+  };
 };
