8bf0603cca84fbc14f8021aed742350551f4a5bd
niallw <niall.whelehan@mttnow.com>
2015-09-01 21:13:41 +0100

    Task - refreshMapView call + test
    * Updated games to set moved units action token to "done"

--- app/models/games.js
+++ app/models/games.js
@@ -253,7 +253,7 @@ GamesSchema.statics = {
         {
           $push: {
             "state.units": {
-              "order": "notSet",
+              "order": "done",
               "tanks": 0,
               "ranged": 0,
               "infantry": 0,
